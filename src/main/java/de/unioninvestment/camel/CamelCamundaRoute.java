0d9b060555a28863a6c80889dd2298c3371cf712
Jörg Larbig <joerg@larbig.net>
2017-09-01 12:04:23 +0200

    Initial version

---
+++ src/main/java/de/unioninvestment/camel/CamelCamundaRoute.java
@@ -0,0 +1,29 @@
+package de.unioninvestment.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelCamundaRoute extends RouteBuilder{
+	
+	@Override
+	public void configure() throws Exception {
+		
+
+		from("timer://camundaStarter?fixedRate=true&period=60000")
+		.id("camel-camunda-starter")
+		.to("log:start_camunda_bpmn_from_camel_route")
+		.setBody(constant("test"))
+		.to("camunda-bpm://start?processDefinitionKey=camunda-camel");
+
+		
+		
+		from("direct://2camel")
+		.id("2camel")
+		.to("log:camunda_bpmn_called_camel_route");
+
+
+	
+
+	}
+}
