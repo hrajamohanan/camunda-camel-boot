978fac2eaeeff2de006fbc2ff895c0ebcf7267c8
Jörg Larbig <joerg@larbig.net>
2017-09-06 13:53:03 +0200

    remove unused imports

--- src/main/java/de/unioninvestment/CamundaCamelBootConfiguration.java
+++ src/main/java/de/unioninvestment/CamundaCamelBootConfiguration.java
@@ -1,22 +1,17 @@
 package de.unioninvestment;
 
-import java.io.IOException;
-
-import javax.sql.DataSource;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.camunda.bpm.camel.component.CamundaBpmComponent;
 import org.camunda.bpm.camel.spring.CamelServiceImpl;
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
+
 import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+
 
 @Configuration
 public class CamundaCamelBootConfiguration {
